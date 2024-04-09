919600561713f1fe3e46d0c7d67f21bf20da3c2d
Imani Honey <lilimani32@yahoo.com>
2024-04-09 16:26:15 -0500

    added homepages, firebase, and sign ins

---
+++ js/thread-data.js
@@ -0,0 +1,9 @@
+function populate() {
+    threads =[];
+
+    var thread1 = {
+        topic: 'What do I do about arguments with my coach?',
+        name: 'johnny'
+    };
+    threads.push(thread1);
+}
