ac2d0a28598185a016e936739a3551b92a32a43b
Imani Honey <lilimani32@yahoo.com>
2024-04-23 17:42:19 -0500

    final

--- js/thread-data.js
+++
@@ -1,9 +0,0 @@
-function populate() {
-    threads =[];
-
-    var thread1 = {
-        topic: 'What do I do about arguments with my coach?',
-        name: 'johnny'
-    };
-    threads.push(thread1);
-}
